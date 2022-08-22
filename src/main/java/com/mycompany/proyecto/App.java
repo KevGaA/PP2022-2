a557c53d43dc9c71130a662e7495c84cd990c063
Orkaan <heldersac01@gmail.com>
2022-08-22 17:38:13 -0400

    comentario v3.0

--- src/main/java/com/mycompany/proyecto/App.java
+++ src/main/java/com/mycompany/proyecto/App.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+//test
 import java.io.IOException;
 import static javafx.application.Application.launch;
 
